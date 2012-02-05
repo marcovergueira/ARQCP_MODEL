a8b5b04806f84c48a2f654a3faa034c23dac5ec2
marcovergueira <git@github.com>
2012-02-05 20:05:58 +0000

    First version.

---
+++ ModeloFuncional/asm.h
@@ -0,0 +1,14 @@
+/*
+ * asm.h
+ *
+ *  Created on: Oct 7, 2011
+ *      Author: 1090623, Marco Vergueira
+ *      		ISEP @ 2011
+ */
+
+#ifndef ASM_H_
+#define ASM_H_
+
+int funcao(int a);
+
+#endif
---
+++ ModeloFuncional/main.c
@@ -0,0 +1,18 @@
+/*
+ * main.c
+ *
+ *  Created on: Oct 30, 2011
+ *      Author: 1090623, Marco Vergueira
+ *      		ISEP @ 2011
+ */
+
+#include <stdio.h>
+#include "asm.h"
+
+int main()
+{
+	printf("Resultado: %d\n", funcao(123));
+
+	printf("Tamanho: %d\n", sizeof(long));
+	return 0;
+}
